31e5402e5083b52c66baa48de85cb71327d58685
Melany200415 <melany.perugachi@epn.edu.ec>
2026-06-27 14:39:56 -0500

    Implementar módulo de inicio de sesión con JavaFX y Scene Builder

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.example.papeleria_proyecto {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.papeleria_proyecto to javafx.fxml;
+    opens com.example.papeleria_proyecto.controller to javafx.fxml;
+
     exports com.example.papeleria_proyecto;
+    exports com.example.papeleria_proyecto.controller;
 }
